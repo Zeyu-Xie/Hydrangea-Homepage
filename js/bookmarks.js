7da5169e464f05b90069eb60c5695a076f44a7e0
acanxie <xie.zeyu20@gmail.com>
2023-09-19 09:28:54 +0800

    .

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -3,7 +3,7 @@ const _bookmarks = document.getElementById("bookmarks")
 fetch("./config.json").then(config => config.json()).then(config => config.bookmarks).then(config => {
     config.forEach(website => {
         const link=document.createElement("li")
-        link.innerHTML=`<a href="${website.website}">${website.title}</a>`
+        link.innerHTML=`<a href="${website.website}"><img src="${website.website+"/favicon.ico"}">${website.title}</a>`
         _bookmarks.appendChild(link)
     });
 }).catch(err => {
