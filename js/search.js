2bcf522569e24c1d08852b19af9ad43e9a475a0a
Acan <xie.zeyu20@gmail.com>
2023-09-19 23:32:51 +0800

    .

--- js/search.js
+++ js/search.js
@@ -5,6 +5,5 @@ let query=""
 
 _search_button.addEventListener("click",()=>{
     query=_search_text.value
-    console.log(query)
     window.open(`https://google.com/search?q=${query}`,"_blank")
 })
